6545b60f6804af9b2cd3def267acc43869a6e847
Liktchay <berseker.rage@hotmail.com>
2021-09-22 21:00:25 +0200

    added sale service interface, implementation and model.

--- Lanitaeshop/LanitaeShop.BusinessLogic/Services/Models/Sale/Sale_ResultSet.cs
+++ Lanitaeshop/LanitaeShop.BusinessLogic/Services/Models/Sale/Sale_ResultSet.cs
@@ -6,6 +6,10 @@ namespace LanitaeShop.BusinessLogic.Services.Models.Sale
 {
     public class Sale_ResultSet
     {
-
+        public int id { get; set; }
+        public int productID { get; set; }
+        public int saleTotalAmount { get; set; }
+        public int personID { get; set; }
+        public DateTime saleDateTime { get; set; }
     }
 }
