c2dde921222198e85c4f42c35fde29ef636eedb6
master mono <berseker.rage@hotmail.com>
2022-03-14 17:53:37 -0300

    added new UI react project

---
+++ Lanitaeshop/lanitaeshop.uinew/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ClothList() {
+    return (
+        <section>
+            Available Clothes
+        </section>
+    );
+}
+
+ReactDOM.render(<ClothList/>, document.getElementById('root'))
